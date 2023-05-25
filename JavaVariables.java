cd0aa6a2b38652764e719fa23885401686bf1a68
Zuber Najam <71356073+ZuberNajam@users.noreply.github.com>
2023-05-25 07:01:24 -0400

    Add files via upload
    updated file commit

--- JavaVariables.java
+++ JavaVariables.java
@@ -22,9 +22,14 @@ public class JavaVariables {
         System.out.println("Please enter your budget in US dollars: $");
         double budget = scanner.nextDouble();
 
+        
+        scanner.nextLine(); // throwaway nextLine to be wasted on empty space
+        System.out.println("\nPlease enter your company name: ");
+        String companyName = scanner.nextLine();
+
         scanner.close();
 
-        System.out.println("Welcome to Starlight Systems " + firstName + " " + lastName + "!");
-        System.out.println("For our members of age " + age +" and with a budget of $" + budget + ", we offer the premium package.");
+        System.out.println("Welcome to Starlight Systems " + firstName + " " + lastName + " from " + companyName + "!");
+        System.out.println("For our members of age " + age +" with a budget of $" + budget + ", we offer the premium package.");
     }
 }
